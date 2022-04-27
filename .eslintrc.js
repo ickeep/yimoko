7e59d3e488313b7f3691912515a86beaa1dad03e
keepcai <keepcai@tencent.com>
2022-04-27 19:28:25 +0800

    项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  extends: ['eslint-config-tencent', 'eslint-config-tencent/ts'],
+  env: {
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: [
+    'react-hooks',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
+    'react-hooks/exhaustive-deps': 'warn', // 检查 effect 的依赖
+    complexity: ['error', 4],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always-and-inside-groups',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+        pathGroupsExcludedImportTypes: [
+          'builtin',
+        ],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+      },
+    ],
+  },
+};
