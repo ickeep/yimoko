e7b99d23fc5b7a5e2a169f29664d7ea3f3be7aa3
keepcai <keepcai@tencent.com>
2022-04-27 22:31:14 +0800

    demo

--- example/console/craco.config.js
+++ example/console/craco.config.js
@@ -1,44 +1,43 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
 
-const alias= {
-  '@/src': path.resolve(__dirname, './src'),
-  '@/tests': path.resolve(__dirname, './tests'),
-}
+const CracoLessPlugin = require('craco-less');
 
-const externals={
+const  externals = {
   react: { amd: 'React', var: 'React', root: 'React', commonjs2: 'react', commonjs: 'react' },
   'react-dom': { amd: 'ReactDOM', var: 'ReactDOM', root: 'ReactDOM', commonjs2: 'react-dom', commonjs: 'react-dom' },
   'react-router-dom': { amd: 'ReactRouterDOM', var: 'ReactRouterDOM', root: 'ReactRouterDOM', commonjs2: 'react-router-dom', commonjs: 'react-router-dom' },
   antd: 'antd',
   moment: 'moment',
   axios: 'axios',
-}
+};
 
-const plugins= [
-  {
-    plugin: CracoLessPlugin,
-    options: {
-      lessLoaderOptions: {
-        lessOptions: {
-          modifyVars: {
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+            },
+            javascriptEnabled: true,
           },
-          javascriptEnabled: true,
         },
       },
     },
-  },
-]
-
-
-module.exports = {
-  plugins,
-  externals,
+  ],
   webpack: {
-    alias,
+    alias: {
+      '@/src': path.resolve(__dirname, './src'),
+      '@/tests': path.resolve(__dirname, './tests'),
+    },
+
     configure: (webpackConfig) => {
       const newConf = webpackConfig;
       newConf.externals = externals;
       if (process.env.NODE_ENV === 'production') {
-        newConf.output.publicPath = 'https://merchant-rsstu-1251135819.file.myqcloud.com/';
+        // newConf.output.publicPath = 'https://merchant-rsstu-1251135819.file.myqcloud.com/';
       }
       const { optimization } = newConf;
       delete optimization.splitChunks;
---
+++ example/console/src/app.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'antd';
+
+export const App = () => (
+  <div className="App">
+    <Button>按钮</Button>
+  </div>
+);
