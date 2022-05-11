374d504e81f7134c63486f9f4f419a0cb13b5179
keepcai <keepcai@tencent.com>
2022-05-11 18:58:50 +0800

    增加单测

--- packages/store/src/base.test.tsx
+++ packages/store/src/base.test.tsx
@@ -2,9 +2,24 @@ import { BaseStore } from './base';
 
 describe('BaseStore', () => {
   const store = new BaseStore({ defaultValues: { name: '' }, api: {} });
-  test('adds 1 + 2 to equal 3', () => {
+  test('setValues', () => {
     store.setValues({ name: 'name' });
     expect(store.values).toEqual({ name: 'name' });
+  });
+
+  test('resetValues', () => {
+    store.resetValues();
+    expect(store.values).toEqual({ name: '' });
+  });
+
+  test('setValuesByField', () => {
+    store.setValuesByField('name', 'name');
+    expect(store.values).toEqual({ name: 'name' });
+  });
+
+  test('resetValuesByFields', () => {
+    store.resetValuesByFields(['name']);
+    expect(store.values).toEqual({ name: '' });
   });
 });
 
