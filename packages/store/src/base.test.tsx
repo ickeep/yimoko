93cb00d69942e4d874fd3a625f87d240ec980679
keepcai <keepcai@tencent.com>
2022-05-17 09:12:14 +0800

    完善测试用例

--- packages/store/src/base.test.tsx
+++ packages/store/src/base.test.tsx
@@ -1,25 +1,28 @@
 import { BaseStore } from './base';
 
 describe('BaseStore', () => {
-  const store = new BaseStore({ defaultValues: { name: '' }, api: {} });
+  const store = new BaseStore({ defaultValues: { id: 1, name: '', type: 't1' }, api: {} });
   test('setValues', () => {
     store.setValues({ name: 'name' });
-    expect(store.values).toEqual({ name: 'name' });
+    expect(store.values).toEqual({ id: 1, name: 'name', type: 't1' });
   });
 
   test('resetValues', () => {
     store.resetValues();
-    expect(store.values).toEqual({ name: '' });
+    expect(store.values).toEqual({ id: 1, name: '', type: 't1' });
   });
 
   test('setValuesByField', () => {
     store.setValuesByField('name', 'name');
-    expect(store.values).toEqual({ name: 'name' });
+    expect(store.values).toEqual({ id: 1, name: 'name', type: 't1' });
   });
 
   test('resetValuesByFields', () => {
     store.resetValuesByFields(['name']);
-    expect(store.values).toEqual({ name: '' });
+    expect(store.values).toEqual({ id: 1, name: '', type: 't1' });
+    store.setValues({ id: 2, name: 'name', type: 't2' });
+    store.resetValuesByFields(['name', 'type']);
+    expect(store.values).toEqual({ id: 2, name: '', type: 't1' });
   });
 });
 
