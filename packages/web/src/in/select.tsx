f814a2fd0a740f951875d07f3cbaf988ab1f9ffd
ickeep <i@ickeep.com>
2022-06-07 21:01:42 +0800

    searchProps

--- packages/web/src/in/select.tsx
+++ packages/web/src/in/select.tsx
@@ -2,28 +2,40 @@ import { Select as TSelect } from '@formily/antd';
 import { observer } from '@formily/reactive-react';
 import { IOptions, IKeys, IOptionsAPISearchConfig, useAPIOptions, useAPISearchOptions, IOptionsAPI } from '@yimoko/store';
 import { Spin, SelectProps as TSelectProps } from 'antd';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 export type SelectProps = TSelectProps & IOptionsProps;
 
 export const Select = observer((props: SelectProps) => {
   const { splitter, keys, options, api, apiType, searchConfig, value, ...args } = props;
   const [searchVal, setSearchVal] = useState('');
+
+
   const [data, loading] = apiType === 'search'
     ? useAPISearchOptions(searchVal, value, options, api, searchConfig, keys, splitter)
     : useAPIOptions(options, api, keys, splitter);
 
+  const searchProps = useMemo(() => {
+    const isApiSearch = apiType === 'search';
+    if (isApiSearch) {
+      return {
+        showSearch: true,
+        filterOption: false,
+        searchValue: searchVal,
+        onSearch: setSearchVal,
+      };
+    }
+    return {};
+  }, [apiType, searchVal]);
+
   return (
     <TSelect
       allowClear={!loading}
-      showSearch={apiType === 'search'}
-      filterOption={apiType !== 'search'}
       notFoundContent={loading ? <Spin size='small' /> : null}
       loading={loading}
       options={data}
+      {...searchProps}
       {...args}
-      searchValue={searchVal}
-      onSearch={apiType === 'search' ? setSearchVal : undefined}
       value={value}
     />
   );
