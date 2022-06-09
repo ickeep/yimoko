8e2e5d5c661a850117405132d7341412fce5a3ce
ickeep <i@ickeep.com>
2022-06-09 20:12:56 +0800

    Layout

--- packages/demo-console/src/page/layout/index.tsx
+++ packages/demo-console/src/page/layout/index.tsx
@@ -1,3 +1,73 @@
+import { Form, FormItem, Input, Password } from '@formily/antd';
+import { createForm } from '@formily/core';
+import { createSchemaField } from '@formily/react';
 import { observer } from '@formily/reactive-react';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+const normalForm = createForm({
+  validateFirst: true,
+});
 
-export const LayoutPage = observer(() => <div>12</div>);
+const normalSchema = {
+  type: 'void',
+  'x-component': 'Layout',
+  properties: {
+    header: {
+      type: 'void',
+      'x-component': 'Header',
+    },
+    content: {
+      type: 'void',
+      'x-component': 'Content',
+      properties: {
+        sider: {
+          type: 'void',
+          'x-component': 'Sider',
+          properties: {
+            sider: {
+              type: 'void',
+              'x-component': 'Input',
+            },
+          },
+        },
+        content: {
+          type: 'void',
+          'x-component': 'Content',
+        },
+      },
+    },
+    footer: {
+      type: 'void',
+      'x-component': 'Footer',
+    },
+  },
+};
+
+const SchemaField = createSchemaField({
+  components: {
+    Layout,
+    FormItem,
+    Input,
+    Password,
+    Header,
+    Footer,
+    Sider,
+    Content,
+  },
+});
+
+export const LayoutPage = observer(() => <>
+  <Form
+    form={normalForm}
+    layout="vertical"
+    size="large"
+    onAutoSubmit={console.log}
+  >
+    <SchemaField schema={normalSchema} />
+  </Form>
+  <Layout>
+    <Header>Header</Header>
+    <Content>Content</Content>
+    <Footer>Footer</Footer>
+  </Layout>
+</>);
