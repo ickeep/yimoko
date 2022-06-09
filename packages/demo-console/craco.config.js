0cc5e4c5eb1aaebdb33bb7ac3c97c3f79289fb17
ickeep <i@ickeep.com>
2022-06-09 15:46:01 +0800

    npm to cos

--- packages/demo-console/craco.config.js
+++ packages/demo-console/craco.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path');
 
+const { getPlugin, pluginByName } = require('@craco/craco/lib/webpack-plugins');
+
 const externals = {
   react: 'React',
   'react-is': 'ReactIs',
@@ -22,7 +24,126 @@ const externals = {
   '@formily/json-schema': 'Formily.JSONSchema',
 };
 
+const cdn = {
+  devCSS: [
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.variable.css',
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.variable.css.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/antd.css',
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/antd.css.map',
+  ],
+  prodCSS: [
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.variable.min.css',
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.variable.min.css.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/antd.css',
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/antd.css.map',
+  ],
+  devJS: [
+    'https://cdn.jsdelivr.net/npm/react@18.1.0/umd/react.development.min.js',
+    'https://cdn.jsdelivr.net/npm/react-is@18.1.0/umd/react-is.development.min.js',
+    'https://cdn.jsdelivr.net/npm/react-dom@18.1.0/umd/react-dom.development.min.js',
+
+    'https://cdn.jsdelivr.net/npm/react-router-dom@6.3.0/umd/react-router-dom.development.js',
+    'https://cdn.jsdelivr.net/npm/react-router-dom@6.3.0/umd/react-router-dom.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.js',
+
+    'https://cdn.jsdelivr.net/npm/moment@2.29.3/min/moment.min.js',
+    'https://cdn.jsdelivr.net/npm/moment@2.29.3/min/moment.min.js.map',
+
+    'https://cdn.jsdelivr.net/npm/moment@2.29.3/locale/zh-cn.js',
+
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.js',
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.js.map',
+
+    'https://cdn.jsdelivr.net/npm/axios@0.27.2/dist/axios.js',
+    'https://cdn.jsdelivr.net/npm/axios@0.27.2/dist/axios.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/path@2.1.4/dist/formily.path.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/path@2.1.4/dist/formily.path.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/shared@2.1.4/dist/formily.shared.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/shared@2.1.4/dist/formily.shared.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/reactive@2.1.4/dist/formily.reactive.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/reactive@2.1.4/dist/formily.reactive.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/validator@2.1.4/dist/formily.validator.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/validator@2.1.4/dist/formily.validator.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/core@2.1.4/dist/formily.core.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/core@2.1.4/dist/formily.core.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/reactive-react@2.1.4/dist/formily.reactive-react.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/reactive-react@2.1.4/dist/formily.reactive-react.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/react@2.1.4/dist/formily.react.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/react@2.1.4/dist/formily.react.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/json-schema@2.1.4/dist/formily.json-schema.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/json-schema@2.1.4/dist/formily.json-schema.umd.development.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@ant-design/icons@4.7.0/dist/index.umd.js',
+
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/formily.antd.umd.development.js',
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/formily.antd.umd.development.js.map',
+  ],
+
+  prodJS: [
+    'https://cdn.jsdelivr.net/npm/react@18.1.0/umd/react.production.min.js',
+    'https://cdn.jsdelivr.net/npm/react-is@18.1.0/umd/react-is.production.min.js',
+    'https://cdn.jsdelivr.net/npm/react-dom@18.1.0/umd/react-dom.production.min.js',
+
+    'https://cdn.jsdelivr.net/npm/react-router-dom@6.3.0/umd/react-router-dom.production.min.js',
+    'https://cdn.jsdelivr.net/npm/react-router-dom@6.3.0/umd/react-router-dom.production.min.js.map',
+
+    'https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js',
+
+    'https://cdn.jsdelivr.net/npm/moment@2.29.3/min/moment.min.js',
+    'https://cdn.jsdelivr.net/npm/moment@2.29.3/min/moment.min.js.map',
+
+    'https://cdn.jsdelivr.net/npm/moment@2.29.3/locale/zh-cn.js',
+
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.min.js',
+    'https://cdn.jsdelivr.net/npm/antd@4.21.0/dist/antd.min.js.map',
+
+    'https://cdn.jsdelivr.net/npm/axios@0.27.2/dist/axios.min.js',
+    'https://cdn.jsdelivr.net/npm/axios@0.27.2/dist/axios.min.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/path@2.1.4/dist/formily.path.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/path@2.1.4/dist/formily.path.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/shared@2.1.4/dist/formily.shared.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/shared@2.1.4/dist/formily.shared.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/reactive@2.1.4/dist/formily.reactive.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/reactive@2.1.4/dist/formily.reactive.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/validator@2.1.4/dist/formily.validator.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/validator@2.1.4/dist/formily.validator.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/core@2.1.4/dist/formily.core.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/core@2.1.4/dist/formily.core.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/reactive-react@2.1.4/dist/formily.reactive-react.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/reactive-react@2.1.4/dist/formily.reactive-react.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/react@2.1.4/dist/formily.react.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/react@2.1.4/dist/formily.react.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@formily/json-schema@2.1.4/dist/formily.json-schema.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/json-schema@2.1.4/dist/formily.json-schema.umd.production.js.map',
+
+    'https://cdn.jsdelivr.net/npm/@ant-design/icons@4.7.0/dist/index.umd.min.js',
+
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/formily.antd.umd.production.js',
+    'https://cdn.jsdelivr.net/npm/@formily/antd@2.1.4/dist/formily.antd.umd.production.js.map',
+  ],
+};
+
 module.exports = {
+  cdn,
   webpack: {
     alias: {
       '@/src': path.resolve(__dirname, './src'),
@@ -46,6 +167,17 @@ module.exports = {
       delete optimization.splitChunks;
       delete optimization.runtimeChunk;
       newConf.optimization = optimization;
+      const { match: htmlWebpackPlugin } = getPlugin(webpackConfig, pluginByName('HtmlWebpackPlugin'));
+      const filter = str => !/.map$/.test(str);
+      const host = 'npm.ickeep.com';
+      const changeHost = str => str.replace(/:\/\/(cdn.jsdelivr.net\/npm|unpkg.com)\//, `://${host}/`).replace(/@(\d+)/, '/$1');
+      if (process.env.NODE_ENV === 'development') {
+        htmlWebpackPlugin.userOptions.cdnCSS = cdn.devCSS.filter(filter).map(changeHost);
+        htmlWebpackPlugin.userOptions.cdnJS = cdn.devJS.filter(filter).map(changeHost);
+      } else {
+        htmlWebpackPlugin.userOptions.cdnCSS = cdn.prodCSS.filter(filter).map(changeHost);
+        htmlWebpackPlugin.userOptions.cdnJS = cdn.prodJS.filter(filter).map(changeHost);
+      }
       return webpackConfig;
     },
   },
---
+++ packages/demo-console/devops/npm-to-cos.ts
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import COS, { UploadBody } from 'cos-nodejs-sdk-v5';
+
+import buildConf from '../craco.config';
+
+let cosConf: Record<string, string> = {};
+const defaultConf: Record<string, string> = {
+  Bucket: 'npm-1251135819',
+  Region: 'ap-guangzhou',
+};
+
+try {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  cosConf = { ...defaultConf, ...require('./config.local.ts').default };
+} catch (e) {
+  cosConf = {
+    ...defaultConf,
+    SecretId: process.env.SECRETID ?? '',
+    SecretKey: process.env.SECRETKEY ?? '',
+  };
+}
+
+const cos = new COS({
+  SecretId: cosConf.SecretId,
+  SecretKey: cosConf.SecretKey,
+});
+
+const upload = (key: string, body: UploadBody) => {
+  cos.putObject({
+    Bucket: cosConf.Bucket,
+    Region: cosConf.Region,
+    Key: key,
+    Body: body,
+  }, (err, data) => {
+    err ? console.error(err) : console.log(data);
+  });
+};
+
+const { devCSS, prodCSS, devJS, prodJS } = buildConf.cdn;
+const files = Array.from(new Set([...devCSS, ...prodCSS, ...devJS, ...prodJS]));
+
+files.forEach((file) => {
+  const key = file
+    .replace('https://cdn.jsdelivr.net/npm/', '')
+    .replace('https://unpkg.com/', '')
+    .replace(/@(\d+)/, '/$1');
+  axios.get(file, { responseType: 'arraybuffer' }).then((res) => {
+    upload(key, res.data);
+  });
+});
