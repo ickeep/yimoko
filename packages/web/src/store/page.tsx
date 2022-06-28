2b47cf8e1ac3824c7189bd524deb031f22ec0c50
keepcai <keepcai@tencent.com>
2022-06-28 20:04:20 +0800

    StorePage

---
+++ packages/web/src/store/page.tsx
@@ -0,0 +1,25 @@
+import { IFormProps } from '@formily/core';
+import { SchemaReactComponents, ISchema } from '@formily/react';
+import { observer } from '@formily/reactive-react';
+import { BaseStore, IStoreConfig, IStoreValues } from '@yimoko/store';
+import { useMemo } from 'react';
+
+import { SchemaPage } from '../schema/page';
+
+export interface StorePageProps<V extends object = IStoreValues, R = IStoreValues> extends React.HTMLAttributes<HTMLDivElement> {
+  store: BaseStore<V, R> | IStoreConfig<V, R>
+  options?: IFormProps,
+  components?: SchemaReactComponents;
+  scope?: any;
+  schema?: ISchema
+}
+
+export const StorePage = observer(StorePageFn);
+
+function StorePageFn<V extends object = IStoreValues, R = IStoreValues>(props: StorePageProps<V, R>) {
+  const { store, options, scope, ...args } = props;
+  const curStore = useMemo(() => (store instanceof BaseStore ? store : new BaseStore(store)), [store]);
+  const { values } = curStore;
+
+  return <SchemaPage options={{ ...options, values }} scope={{ ...scope, curStore }}  {...args} />;
+}
