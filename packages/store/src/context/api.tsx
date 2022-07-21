13130b9425f71e440af5f05770e31b8d1c8b096b
keepcai <keepcai@tencent.com>
2022-07-21 19:51:58 +0800

    APIExecutor 单元测试

--- packages/store/src/context/api.tsx
+++ packages/store/src/context/api.tsx
@@ -4,9 +4,13 @@ import { IAPIRequestConfig, IHTTPCode, IHTTPResponse } from '../tools/api';
 
 export type IAPIExecutor = <T extends object = IAPIRequestConfig> (config: T) => Promise<IHTTPResponse>;
 
-export const APIExecutorContext = createContext<IAPIExecutor>(() => Promise.resolve({
-  code: IHTTPCode.networkError, msg: 'APIContext 未配置', data: '',
-}));
+export const unknownAPIRes = {
+  code: IHTTPCode.networkError,
+  msg: 'APIContext 未配置',
+  data: '',
+};
+
+export const APIExecutorContext = createContext<IAPIExecutor>(() => Promise.resolve(unknownAPIRes));
 
 export const APIExecutorProvider = APIExecutorContext.Provider;
 
