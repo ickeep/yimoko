f2eda8abb62a46daf8cd9f6883a82ad2e923902c
ickeep <i@ickeep.com>
2022-09-20 15:04:44 +0800

    modal 支持 fn

--- packages/demo-console/src/page/store/page-list.tsx
+++ packages/demo-console/src/page/store/page-list.tsx
@@ -1,176 +1,145 @@
 import { Submit } from '@formily/antd';
 import { observer } from '@formily/react';
 import { useListStore, useOperateStore } from '@yimoko/store';
-import { Drawer, Icon, StoreForm, StorePage, StorePageContent, StoreTable } from '@yimoko/web';
+import { Drawer, Icon, Modal, StoreForm, StorePage, StorePageContent, StoreTable } from '@yimoko/web';
 import { Space } from 'antd';
 
 
-// export const StorePageList = observer(() => {
-//   const store = useOperateStore({
-//     fieldsConfig: {
-//       type: { title: '类型', type: 'string', 'x-component': 'Input', 'x-decorator': 'FormItem', column: { width: 130 } },
-//       name: { type: 'string', title: '名称', 'x-component': 'Input', 'x-decorator': 'FormItem', required: true },
-//     },
-//     // type: 'list',
-//     dictConfig: [
-//       { field: 'type', data: [{ value: 't1', label: '类型1' }, { value: 't2', label: '类型2' }] },
-//       // { field: 'name', data: [{ value: 'n1', label: '名字1' }, { value: 'n2', label: '名字2' }] },
-//     ],
-//     defaultValues: { type: '', name: '' },
-//     api: { url: '/api/page/list' },
-//     // isRunNow: false,
-//     // isBindSearch: false,
-//   });
-//   return (
+export const StorePageList = observer(() => {
+  const store = useOperateStore({
+    fieldsConfig: {
+      type: { title: '类型', type: 'string', 'x-component': 'Input', 'x-decorator': 'FormItem', column: { width: 130 } },
+      name: { type: 'string', title: '名称', 'x-component': 'Input', 'x-decorator': 'FormItem', required: true },
+    },
+    // type: 'list',
+    dictConfig: [
+      { field: 'type', data: [{ value: 't1', label: '类型1' }, { value: 't2', label: '类型2' }] },
+      // { field: 'name', data: [{ value: 'n1', label: '名字1' }, { value: 'n2', label: '名字2' }] },
+    ],
+    defaultValues: { type: '', name: '' },
+    api: { url: '/api/page/list' },
+    // isRunNow: false,
+    // isBindSearch: false,
+  });
+  return (
+    <Modal closeBox={false} isBindStore title="新增" trigger={{ children: 'xxx' }} >
+      <StorePage store={store} >
+        <StoreForm fields={['name']}>
+          <Space>
+            <Submit>查询</Submit>
+          </Space>
+        </StoreForm>
+        {/* <StorePageContent >
+        <StoreTable
+          isControlled={false}
+          store={store}
+          expandable={{
+            isTitleControlsAll: true,
+            expandedRowRender: record => <p style={{ margin: 0 }}>{record.name}</p>,
+            icon: {
+              expanded: { name: 'RightOutlined', style: { color: 'blue' } },
+              collapsed: 'DownOutlined',
+            },
+          }}
+          columns={[
+            { dataIndex: 'id', autoSorter: 'number', fixed: 'left' },
+            { dataIndex: 'name', autoFilter: true },
+            { dataIndex: 'type', autoFilter: true },
+            { dataIndex: 'date', title: 'date', autoSorter: 'date' },
+            { dataIndex: 'time', title: 'time', autoSorter: 'time' },
+            { dataIndex: 'percentage', title: 'percentage', autoSorter: 'percentage' },
+            { dataIndex: 'zh', title: 'zh', autoSorter: 'string', sorterParams: 'zh' },
+            { dataIndex: 'length', title: 'length', autoSorter: 'length' },
+          ]} />
+      </StorePageContent> */}
+      </StorePage>
+    </Modal>
+  );
+});
 
-//     <StorePage store={store} >
-//       <StoreForm fields={['name']}>
-//         <Space>
-//           <Submit>查询</Submit>
-//         </Space>
-//       </StoreForm>
-//       {/* <StorePageContent >
-//         <StoreTable
-//           isControlled={false}
-//           store={store}
-//           expandable={{
-//             isTitleControlsAll: true,
-//             expandedRowRender: record => <p style={{ margin: 0 }}>{record.name}</p>,
-//             icon: {
-//               expanded: { name: 'RightOutlined', style: { color: 'blue' } },
-//               collapsed: 'DownOutlined',
+// export const StorePageList = observer(() => (
+//   <StorePage
+//     store={{}}
+//     options={{ validateFirst: true }}
+//     schema={{
+//       type: 'object',
+//       definitions: {},
+//       properties: {
+//         title: { type: 'void', 'x-component': 'Title', 'x-component-props': { children: '列表页' } },
+//         form: {
+//           type: 'void', 'x-component': 'StoreForm',
+//           'x-component-props': { fields: ['type', { field: 'name', title: 'xxx' }] },
+//           properties: {
+//             // type: { $ref: '#/definitions/type' },
+//             // name: { $ref: '#/definitions/name' },
+//             buttons: {
+//               type: 'void', 'x-component': 'Space',
+//               properties: {
+//                 submit: { type: 'void', 'x-component': 'Submit', 'x-component-props': { children: '查询' } },
+//                 reset: { type: 'void', 'x-component': 'Reset', 'x-component-props': { children: '重置' } },
+//               },
 //             },
-//           }}
-//           columns={[
-//             { dataIndex: 'id', autoSorter: 'number', fixed: 'left' },
-//             { dataIndex: 'name', autoFilter: true },
-//             { dataIndex: 'type', autoFilter: true },
-//             { dataIndex: 'date', title: 'date', autoSorter: 'date' },
-//             { dataIndex: 'time', title: 'time', autoSorter: 'time' },
-//             { dataIndex: 'percentage', title: 'percentage', autoSorter: 'percentage' },
-//             { dataIndex: 'zh', title: 'zh', autoSorter: 'string', sorterParams: 'zh' },
-//             { dataIndex: 'length', title: 'length', autoSorter: 'length' },
-//           ]} />
-//       </StorePageContent> */}
-//     </StorePage>
+//           },
+//         },
 
-//   );
-// });
-
-export const StorePageList = observer(() => (
-  <StorePage
-    store={{}}
-    options={{ validateFirst: true }}
-    schema={{
-      type: 'object',
-      definitions: {},
-      properties: {
-        title: { type: 'void', 'x-component': 'Title', 'x-component-props': { children: '列表页' } },
-        form: {
-          type: 'void', 'x-component': 'StoreForm',
-          'x-component-props': { fields: ['type', { field: 'name', title: 'xxx' }] },
-          properties: {
-            // type: { $ref: '#/definitions/type' },
-            // name: { $ref: '#/definitions/name' },
-            buttons: {
-              type: 'void', 'x-component': 'Space',
-              properties: {
-                submit: { type: 'void', 'x-component': 'Submit', 'x-component-props': { children: '查询' } },
-                reset: { type: 'void', 'x-component': 'Reset', 'x-component-props': { children: '重置' } },
-              },
-            },
-          },
-        },
-        page: {
-          type: 'void',
-          'x-component': 'SchemaTemplate',
-          'x-component-props': {
-            schema: {
-              type: 'object',
-              properties: {
-                page: {
-                  type: 'void',
-                  'x-component': 'StorePage',
-                  'x-component-props': {
-                    store: {
-                      defaultValues: { name: 'name' },
-                    },
-                  },
-                  properties: {
-                    form: {
-                      type: 'void',
-                      'x-component': 'StoreForm',
-                      'x-component-props': {
-                        fields: ['name'],
-                      },
-                      properties: {
-                        name: { type: 'string', title: '名称', 'x-component': 'Input', 'x-decorator': 'FormItem', required: true },
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-        content: {
-          type: 'void',
-          'x-component': 'StorePageContent',
-          'x-component-props': {
-          },
-          properties: {
-            table: {
-              type: 'void',
-              'x-component': 'StoreTable', 'x-decorator': 'RedirectListData',
-              'x-component-props': {
-                isControlled: false,
-                rowSelection: { fixed: true },
-                expandable: {
-                  isTitleControlsAll: true,
-                  icon: {
-                    expanded: { name: 'RightOutlined', style: { color: 'blue' } },
-                    // expanded: 'RightOutlined',
-                    collapsed: 'DownOutlined',
-                  },
-                  expandedRowRender: (record: any) => <div>{record.id}</div>,
-                },
-                columns: ['name',
-                  { dataIndex: 'tag', width: 200, autoFilter: true, isFilterContains: true },
-                  { dataIndex: 'arr', width: 200, autoFilter: true, isFilterContains: true },
-                  {
-                    title: '合并', children: [
-                      { dataIndex: 'name', sorter: true, autoFilter: true },
-                      { dataIndex: 'id', autoSorter: 'number' },
-                    ],
-                  }],
-              },
-              items: [{
-                type: 'string',
-                name: 'id',
-                title: 'ID',
-                'x-decorator-props': { autoSorter: 'number', width: 300, autoFilter: true },
-              },
-              { name: 'name' },
-              {
-                $ref: '#/definitions/name',
-                type: 'void', 'x-component': 'Space',
-                'x-decorator-props': {
-                  autoSorter: 'number', width: 600,
-                },
-                properties: {
-                  name: {
-                    type: 'string', 'x-component': 'Link',
-                    'x-component-props': { href: 'https://www.baidu.com', copyable: true },
-                    // 'x-component-props': { children: '{{$record.name +"-"+ $index+$records[0].name}}' },
-                  },
-                },
-              },
-              { name: 'type', 'x-decorator-props': { autoFilter: true } },
-              ],
-            },
-          },
-        },
-      },
-    }}
-  />
-));
+//         content: {
+//           type: 'void',
+//           'x-component': 'StorePageContent',
+//           'x-component-props': {
+//           },
+//           properties: {
+//             table: {
+//               type: 'void',
+//               'x-component': 'StoreTable', 'x-decorator': 'RedirectListData',
+//               'x-component-props': {
+//                 isControlled: false,
+//                 rowSelection: { fixed: true },
+//                 expandable: {
+//                   isTitleControlsAll: true,
+//                   icon: {
+//                     expanded: { name: 'RightOutlined', style: { color: 'blue' } },
+//                     // expanded: 'RightOutlined',
+//                     collapsed: 'DownOutlined',
+//                   },
+//                   expandedRowRender: (record: any) => <div>{record.id}</div>,
+//                 },
+//                 columns: ['name',
+//                   { dataIndex: 'tag', width: 200, autoFilter: true, isFilterContains: true },
+//                   { dataIndex: 'arr', width: 200, autoFilter: true, isFilterContains: true },
+//                   {
+//                     title: '合并', children: [
+//                       { dataIndex: 'name', sorter: true, autoFilter: true },
+//                       { dataIndex: 'id', autoSorter: 'number' },
+//                     ],
+//                   }],
+//               },
+//               items: [{
+//                 type: 'string',
+//                 name: 'id',
+//                 title: 'ID',
+//                 'x-decorator-props': { autoSorter: 'number', width: 300, autoFilter: true },
+//               },
+//               { name: 'name' },
+//               {
+//                 $ref: '#/definitions/name',
+//                 type: 'void', 'x-component': 'Space',
+//                 'x-decorator-props': {
+//                   autoSorter: 'number', width: 600,
+//                 },
+//                 properties: {
+//                   name: {
+//                     type: 'string', 'x-component': 'Link',
+//                     'x-component-props': { href: 'https://www.baidu.com', copyable: true },
+//                     // 'x-component-props': { children: '{{$record.name +"-"+ $index+$records[0].name}}' },
+//                   },
+//                 },
+//               },
+//               { name: 'type', 'x-decorator-props': { autoFilter: true } },
+//               ],
+//             },
+//           },
+//         },
+//       },
+//     }}
+//   />
+// ));
--- packages/web/src/out/modal.tsx
+++ packages/web/src/out/modal.tsx
@@ -1,7 +1,8 @@
 import { observer } from '@formily/react';
 import { BoxContentProvider, IStore, useBoxStore, BoxContentRender, useBoxContent, useCurForm, judgeIsEmpty } from '@yimoko/store';
 import { Modal as AModal, ModalProps as AModalProps } from 'antd';
-import { Key, ReactElement, FC, Component, useMemo } from 'react';
+import { ModalFunc } from 'antd/lib/modal/confirm';
+import { Key, ReactElement, FC, Component, useMemo, useEffect, useRef } from 'react';
 
 import { trigStoreRun } from './run-trigger';
 import { ITriggerRender, Trigger, TriggerProps } from './trigger';
@@ -12,7 +13,7 @@ interface IContentProps {
   [key: Key]: any
 }
 
-export interface ModalProps extends Omit<AModalProps, 'children' | 'footer'> {
+export interface ModalProps extends Omit<AModalProps, 'children'> {
   trigger?: ITriggerRender
   trigEvent?: TriggerProps['trigEvent']
   content?: ReactElement<IContentProps> | FC<IContentProps> | Component<IContentProps>
@@ -23,6 +24,7 @@ export interface ModalProps extends Omit<AModalProps, 'children' | 'footer'> {
   isBindContent?: boolean
   store?: IStore
   closeBox?: boolean | 'success' | 'fail'
+  type?: 'info' | 'success' | 'error' | 'warning' | 'confirm'
 }
 
 export const Modal = observer((props: ModalProps) => {
@@ -65,14 +67,44 @@ const ModalRender = observer((props: Omit<ModalProps, 'trigger' | 'onOpen' | 'on
   };
 
   return (
-    <AModal
+    <ModalTypeRender
       {...args}
       okButtonProps={{ loading: curStore?.loading, disabled: !judgeIsEmpty(curForm?.errors), ...okButtonProps }}
       onOk={ok}
       onCancel={cancel}
     >
       <BoxContentRender isBind={isBindContent} content={content}>{children}</BoxContentRender>
-    </AModal>
+    </ModalTypeRender>
   );
+});
+
+const ModalTypeRender = observer((props: AModalProps & { type?: ModalProps['type'] }) => {
+  const { type, ...args } = props;
+  if (!type) {
+    return <AModal {...args} />;
+  }
+  return <ModalFnRender {...args} type={type} />;
+});
+
+const ModalFnRender = observer((props: AModalProps & { type: Required<ModalProps>['type'] }) => {
+  const [modal, contextHolder] = AModal.useModal();
+  const { type, open, children, ...args } = props;
+  const refModal = useRef<ReturnType<ModalFunc>>();
+
+  useEffect(() => {
+    const m = refModal.current;
+    m && open && m.update({ ...args, content: children });
+  }, [args, children, open]);
+
+  useEffect(() => {
+    if (open) {
+      refModal.current = modal[type]?.({ ...args, content: children });
+    } else {
+      refModal.current?.destroy();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [modal, open, type]);
+
+  return <>{contextHolder}</>;
 });
 
