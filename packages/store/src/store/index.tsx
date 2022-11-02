a0fccd6662b8c429e659c2f32519ff8e98436a8d
ickeep <i@ickeep.com>
2022-11-02 17:47:08 +0800

    完善类型导出

--- packages/store/src/store/index.tsx
+++ packages/store/src/store/index.tsx
@@ -4,10 +4,10 @@ import { IOperateStoreConfig, OperateStore } from './operate';
 
 export type IStoreType = 'base' | 'list' | 'operate';
 
-export type IStore<V extends object = any, R = any> = BaseStore<V, R> | ListStore<V, R> | OperateStore<V, R>;
+export type IStore<V extends object = any, R extends object = any> = BaseStore<V, R> | ListStore<V, R> | OperateStore<V, R>;
 
 // eslint-disable-next-line max-len
-export type IStoreConfig<V extends object = any, R = any> = { type?: IStoreType } & (IBaseStoreConfig<V, R> | IListStoreConfig<V, R> | IOperateStoreConfig<V, R>);
+export type IStoreConfig<V extends object = any, R extends object = any> = { type?: IStoreType } & (IBaseStoreConfig<V, R> | IListStoreConfig<V, R> | IOperateStoreConfig<V, R>);
 
 export const StoreMap = {
   base: BaseStore,
